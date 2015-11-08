f6db2f8a56bf81c117c35dacc81f7e6a96dc581e
hdvinh361989 <vinhux@gmail.com>
2015-11-08 11:19:18 +0700

    First commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by vinhhoang on 08/11/2015.
+ */
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My SECOND Angular 2 App</h1>'
+})
+class AppComponent {
+}
+
+bootstrap(AppComponent);
